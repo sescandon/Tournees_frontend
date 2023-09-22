ea473be4ce3dae000147a6acc5b2e7bd708dcef5
sescandon <sescandon@unal.edu.co>
2023-09-21 23:12:59 -0500

    Protected Routes added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState, useContext } from "react";
-import { registerRequest } from "../api/auth";
+import { createContext, useState, useContext, useEffect } from "react";
+import { loginRequest, registerRequest, verifyTokenRequest } from "../api/auth";
+import Cookies from "js-cookie";
 
 export const AuthContext = createContext();
 
@@ -15,6 +16,7 @@ export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isAunthenticated, setIsAuthenticated] = useState(false);
   const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const signup = async (user) => {
     try {
@@ -27,8 +29,61 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
+  const signin = async (user) => {
+    try {
+      const res = await loginRequest(user);
+      console.log(res);
+      setUser(res.data);
+      setIsAuthenticated(true);
+    } catch (error) {
+      if (Array.isArray(error.response.data)) {
+        return setErrors(error.response.data);
+      }
+      setErrors([error.response.data.message]);
+    }
+  };
+
+  useEffect(() => {
+    if (errors.length > 0) {
+      const timer = setTimeout(() => {
+        setErrors([]);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [errors]);
+
+  useEffect(() => {
+    async function checkLogin() {
+      const cookies = Cookies.get();
+      if (!cookies.token) {
+        setIsAuthenticated(false);
+        setLoading(false);
+        setUser(null);
+      }
+      try {
+        const res = await verifyTokenRequest(cookies.token);
+        if (!res.data) {
+          setIsAuthenticated(false);
+          setLoading(false);
+          setUser(null);
+          return;
+        }
+        setIsAuthenticated(true);
+        setUser(res.data);
+        setLoading(false);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser(null);
+        setLoading(false);
+      }
+    }
+    checkLogin();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ signup, user, isAunthenticated, errors }}>
+    <AuthContext.Provider
+      value={{ signup, loading, signin, user, isAunthenticated, errors }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { Link } from "react-router-dom";
 
 function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const {signin, errors: SigninErrors} = useAuth()
+
+  const onSubmit = handleSubmit((data) => {
+    signin(data);
+  });
+
   return (
-    <div>LoginPage</div>
-  )
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+      {
+        SigninErrors.map((error,i) => (
+          <div className="bg-red-500 p-2 text-white" key={i}>
+            {error}
+          </div>
+        ))
+      }
+        <h1 className="text-2xl font-bold">Login</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="email"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          {...register("email", { required: true })}
+          placeholder="Email"
+        />
+        {errors.email && <p className="text-red-500">Email is required</p>}
+        <input
+          type="password"
+          className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2"
+          {...register("password", { required: true })}
+          placeholder="Password"
+        />
+        {errors.password && (
+          <p className="text-red-500">Password is required</p>
+        )}
+        <button type="submit">Login</button>
+      </form>
+      <p className="flex gap-x-2 justify-between">
+        Don't have an account? <Link to="/register" className="text-sky-500">Sign Up</Link>
+      </p>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
